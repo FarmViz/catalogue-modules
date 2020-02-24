08ad4885f6c357dca578c1fc3bc5abbd394de0e4
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2020-02-24 19:43:49 +0100

    update

--- front/src/components/Email/Contact/Contact.jsx
+++ front/src/components/Email/Contact/Contact.jsx
@@ -1,157 +1,346 @@
 import React, { Component } from 'react';
 import Recaptcha from 'react-google-invisible-recaptcha';
-import './Contact.css';
+
 const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS;
 
 class Contact extends Component {
-    constructor() {
-        super();
-        this.state = {
-            nom: "",
-            prenom: "",
-            telephone: "",
-            email: "",
-            description: "",
-            asLoading: false
-        }
-        this.handlerChange = this.handlerChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: '',
+      lastname: '',
+      email: '',
+      phonenumber: '',
+      message: '',
+      isSent: false,
+      loading: false,
+      formErrors: {
+        email: '',
+        phonenumber: '',
+        lastname: '',
+        firstname: '',
+        message: ''
+      },
+      lastnamevalid: false,
+      firstnamevalid: false,
+      emailvalid: false,
+      messagevalid: false,
+      formvalid: false,
+      captcha: false,
+      contactData: [],
+      phonetel: ''
+    };
+  }
+  componentDidMount() {
+    this.getContact();
+  }
 
-        // captcha
-        this.onSubmit = this.onSubmit.bind( this );
-        this.onResolved = this.onResolved.bind( this );
-        // fin captcha
+  // Récupération des données de la table contact
+  getContact = () => {
+    fetch(SERVER_ADDRESS + '/contact', {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(data => {
+        let noDots = data[0].phone.split('.').join('');
+        let removeFirstCharacter = 'tel:+33' + noDots.substring(1);
+        this.setState({ phonetel: removeFirstCharacter });
+        this.setState({ contactData: data[0] });
+      })
+      .then(data => {});
+  };
+
+  // rentre les données dans le state
+  handleChangeContact = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({ [name]: value }, () => {
+      this.validateField(name, value);
+    });
+  };
+
+  // vérifie les données entrées
+  validateField(fielname, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailvalid = this.state.emailvalid;
+    let lastnamevalid = this.state.lastnamevalid;
+    let firstnamevalid = this.state.firstnamevalid;
+    let messagevalid = this.state.messagevalid;
+
+    switch (fielname) {
+      case 'firstname':
+        firstnamevalid = value.length > 0;
+
+        fieldValidationErrors.firstname = firstnamevalid
+          ? ''
+          : '* Champ requis.';
+        break;
+      case 'lastname':
+        lastnamevalid = value.length > 0;
+        fieldValidationErrors.lastname = lastnamevalid ? '' : '* Champ requis.';
+        break;
+      case 'email':
+        emailvalid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailvalid ? '' : '* email non valide.';
+        break;
+      case 'message':
+        messagevalid = value.length > 0;
+        fieldValidationErrors.message = messagevalid ? '' : '* Champ requis.';
+        break;
+      default:
+        break;
     }
-    handlerChange(event) {
-        console.log(event.target.name);
-        switch (event.target.name) {
-            case 'Nom':
-                this.setState({ nom: event.target.value });
-                break;
-            case 'Prenom':
-                this.setState({ prenom: event.target.value });
-                break;
-            case 'telephone':
-                this.setState({ telephone: event.target.value });
-                break;
-            case 'email':
-                this.setState({ email: event.target.value });
-                break;
-            case 'description':
-                this.setState({ description: event.target.value });
-                break;
-            default:
-                break;
-        }
-    }
-    snackbarClose = (event) => {
-        this.setState({ snackbaropen: false })
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        // if (this.state.recaptcha.execute()) {
+    this.setState(
       {
-            this.setState({ isLoading: true })
-            console.log(this.state)
-            fetch(SERVER_ADDRESS + '/contact',
-                {
-                    method: 'POST',
-                    headers: new Headers({
-                        'Content-Type': 'application/json'
-                    }),
-                    body: JSON.stringify(this.state),
-                })
-                .then(res => res.json())
-                .then(
-                    res => this.setState({ snackbarmessage: res.message }),
-                )
-                .then(
-                    () => this.setState({ isLoading: false })
-                )
-                .then(
-                    () => this.setState({ asLoading: true })
-                )
-        }
-    // }
+        formErrors: fieldValidationErrors,
+        emailvalid: emailvalid,
+        firstnamevalid: firstnamevalid,
+        lastnamevalid: lastnamevalid,
+        messagevalid: messagevalid
+      },
+      this.validateForm
+    );
+  }
+
+  validateForm() {
+    this.setState({
+      formvalid:
+        this.state.emailvalid &&
+        this.state.lastnamevalid &&
+        this.state.firstnamevalid &&
+        this.state.messagevalid
+    });
+  }
+
+  //début fonction
+  handleContactSubmit = event => {
+    event.preventDefault();
+    //errors
+
+    let fieldValidationError = this.state.formErrors;
+
+    fieldValidationError.firstname = this.state.firstnamevalid
+      ? ''
+      : '* Champ requis.';
+
+    fieldValidationError.lastname = this.state.lastnamevalid
+      ? ''
+      : '* Champ requis.';
+
+    fieldValidationError.email = this.state.emailvalid
+      ? ''
+      : '* email non valide.';
+
+    fieldValidationError.message = this.state.messagevalid
+      ? ''
+      : '* champs requis';
+
+    this.setState({ formErrors: fieldValidationError });
+
+    // execution du captcha
+    this.recaptcha.execute();
+
+    //fetch sendMail
+  };
+
+  // le captcha appelle cette fonction après vérification, on exécute onFetch('envoie de mail)
+  onResolved = () => {
+    this.setState({ captcha: true }, this.onFetch);
+  };
+
+  // envoi de mail
+  onFetch = () => {
+    if (
+      this.state.lastnamevalid &&
+      this.state.firstnamevalid &&
+      this.state.emailvalid &&
+      this.state.messagevalid
+    ) {
+      const { phonenumber, lastname, email, firstname, message } = this.state;
+
+      if (this.state.captcha) {
+        this.setState({ loading: true });
+        fetch(SERVER_ADDRESS + '/contact', {
+          method: 'POST',
+          headers: new Headers({
+            'Content-Type': 'application/json'
+          }),
+          body: JSON.stringify({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            phonenumber: phonenumber,
+            message: message
+          })
+        }).then(res => {
+          if (res.ok) {
+            // message de confirmation
+            this.setState({ isSent: true }, () => {
+              setTimeout(() => {
+                this.setState({ isSent: false });
+              }, 5000);
+            });
+
+            // Reset
+
+            this.setState({ loading: false });
+            this.setState({ captcha: false });
+            this.setState({ firstname: '' });
+            this.setState({ lastname: '' });
+            this.setState({ email: '' });
+            this.setState({ message: '' });
+            this.setState({ phonenumber: '' });
+            return res.json();
+          } else {
+            return res.text().then(text => {
+              throw new Error(text);
+            });
+          }
+        });
+      }
     }
+  };
 
-    onSubmit() {
-        if ( '' == this.state.nom ) {
-          alert( 'Erreur de validation! Renseigné les champs obligatoires.' );
-          this.recaptcha.reset();
-        } else {
-          this.recaptcha.execute();
-        }
-      }
-      onResolved() {
-        alert( 'Recaptcha response du captcha: ' + this.recaptcha.getResponse() );
-      }
-  
-    render() {
-        return (
-            <div className="sticky-wrap">
-              
-                <div className="formulaire">
-                    <form className="container align-containner-recrutement" onSubmit={this.handleSubmit}>
-                        <div className="max-wi">
+  //fin fonction
 
-                            <div className="center-we-join"><h1>Nous contacter</h1></div>
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">Nom</div>
-                                    </div>
-                                    <input type="text" name="Nom" placeholder="Nom" className="form-control" onChange={this.handlerChange} />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">Prénom</div>
-                                    </div>
-                                    <input type="text" name="Prenom" placeholder="Prenom" className="form-control" onChange={this.handlerChange} />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">Téléphone</div>
-                                    </div>
-                                    <input type="text" name="telephone" className="form-control" placeholder="numéro de téléphone" onChange={this.handlerChange} />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <div className="input-group">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">Email</div>
-                                    </div>
-                                    <input type="email" name="email" className="form-control" placeholder="exemple@email.fr" onChange={this.handlerChange} />
-                                </div>
-                            </div>
+  render() {
+    const { lastname, firstname, email, phonenumber, message } = this.state;
+    return (
+      <>
+        <div>
+          <div className="container_back_contact">
+            <div className="space_contact"></div>
 
-                            <div className="form-group">
-                                <div className="input-group-text">Message</div>
-                                <textarea name="description" className="form-control" id="exampleFormControlTextarea1" placeholder="Description de votre message" rows="3" onChange={this.handlerChange}></textarea>
-                            </div>
-                            <div className="captcha row justify-content-center">
-                            <Recaptcha
-                               ref={ ref => this.recaptcha = ref }
-                               sitekey="6LfoYdkUAAAAACtar5gBLA7VWg5r-gmxZ-ydwk1o"
-                               onResolved={ this.onResolved }
-                             />
-                            </div>
-                          
-                             <button className="btn-envoyer btn btn-primary" type="submit" onClick={this.onSubmit}>Envoyer</button>
-
+            {/* Contact Part */}
+            <div className="d-flex justify-content-center col-12 mt-5">
+              <section className="bloc_form p-0 col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
+                <div className="container-fluid">
+                  <div className="container">
+                    <div className="formBox">
+                      <form onSubmit={this.handleContactSubmit}>
+                        <div className="row">
+                          <div className="col-sm-12 title_contact mb-5 mt-5">
+                            <h1 className="title_form font-Courgette-contact">
+                              Contact
+                            </h1>
+                          </div>
                         </div>
-                    </form>
+                        <div className="row">
+                          <div className="col-sm-6">
+                            <div className="inputBox focus">
+                              <div className="inputText font-Courgette-contact">
+                                Nom{' '}
+                                <span>{this.state.formErrors.lastname}</span>
+                              </div>
+                              <input
+                                type="text"
+                                name="lastname"
+                                value={lastname}
+                                onChange={this.handleChangeContact}
+                                className="input_form "
+                              />
+                            </div>
+                          </div>
+                          <div className="col-sm-6">
+                            <div className="inputBox focus">
+                              <div className="inputText font-Courgette-contact">
+                                Prénom{' '}
+                                <span>{this.state.formErrors.firstname}</span>
+                              </div>
+                              <input
+                                type="text"
+                                name="firstname"
+                                value={firstname}
+                                onChange={this.handleChangeContact}
+                                className="input_form "
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="row">
+                          <div className="col-sm-6">
+                            <div className="inputBox focus">
+                              <div className="inputText font-Courgette-contact">
+                                Email <span>{this.state.formErrors.email}</span>
+                              </div>
+                              <input
+                                type="text"
+                                name="email"
+                                value={email}
+                                onChange={this.handleChangeContact}
+                                className="input_form"
+                              />
+                            </div>
+                          </div>
+                          <div className="col-sm-6">
+                            <div className="inputBox focus">
+                              <div className="inputText font-Courgette-contact">
+                                Numéro de téléphone
+                              </div>
+                              <input
+                                type="text"
+                                name="phonenumber"
+                                value={phonenumber}
+                                onChange={this.handleChangeContact}
+                                className="input_form"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="row">
+                          <div className="col-sm-12">
+                            <div className="inputBox focus">
+                              <div className="inputText font-Courgette-contact">
+                                Message{' '}
+                                <span>{this.state.formErrors.message}</span>
+                              </div>
+                              <textarea
+                                name="message"
+                                value={message}
+                                onChange={this.handleChangeContact}
+                                className="input_form textarea_form"
+                              ></textarea>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-sm-12 mb-5 button-text-contact">
+                            <input
+                              type="submit"
+                              value={
+                                this.state.loading
+                                  ? 'Envoi en cours...'
+                                  : 'Envoyer'
+                              }
+                              className="button_form"
+                            />
+                          </div>
+                        </div>
+                        <Recaptcha
+                          ref={ref => (this.recaptcha = ref)}
+                          sitekey="6LfSX9YUAAAAAHGi5gUy5NetwOCc0SQe_1YBK8vC"
+                          onResolved={this.onResolved}
+                        />
+                        <div
+                          className={
+                            this.state.isSent ? 'alert alert-success' : 'd-none'
+                          }
+                        >
+                          {this.state.isSent ? 'Message envoyé!' : ''}
+                        </div>
+                      </form>
+                    </div>
+                  </div>
                 </div>
-                <div className="sticky-footer">
-                  
-                </div>
-            </div >
-        )
-    }
+              </section>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Contact;

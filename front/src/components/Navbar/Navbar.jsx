16b9f975e7e68e55f9e9fc72ec4c0a1dd3defb89
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2020-02-24 09:32:47 +0100

    update

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,37 +1,26 @@
 import React from "react";
 import "./Navbar.css"
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
 class Navbar extends React.Component {
 
     render(){
-        return (
-            <div className="topnav" id="myTopnav">
-            <a href="#home" class="active">Home</a>
-            <a href="#news">News</a>
-            <a href="#contact">Contact</a>
-            <div className="dropdown">
-              <button className="dropbtn">Dropdown 
-                <i className="fa fa-caret-down"></i>
-              </button>
-              <div className="dropdown-content">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-              </div>
-            </div> 
-            <a href="#about">About</a>
-            <a href="javascript:void(0);" className="icon" onclick="myFunction()">&#9776;</a>
-          </div>
-         
+        return (  
+   <nav className="header off-1">
+     <h2 className="logo">FARMVIZ SAS</h2>
+    <input type="checkbox" id="chk"/>
+    <label htmlFor="chk" className="show-menu-btn">
+    <i className="fas fa-bars"></i>
+    </label>
+    <ul className="menu dsk-6">
+    <li><a href="/">Accueil</a></li>  
+     <li><a href="/">Services</a></li> 
+    <li><a href="/">Société</a></li>  
+     <li><a href="/">Contact</a></li> 
+      <label htmlFor="chk" className="hide-menu-btn">
+        <i className="fas fa-times"></i>
+      </label>
+    </ul>
+  </nav>   
         )
     }
 }

397d9e8c1e0039a10f5b7ad83e15411c4b09259d
Amaury LEBOUTEUX <amaury.lebouteux@gmail.com>
2020-02-25 12:24:26 +0100

    test hello composant et son import dans app.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,48 +1,27 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-// import Devis from './components/email/Devis';
-import Recrutement from './components/Email/Recrutement/Recrutement';
-import Contact from "./components/Email/Contact/Contact";
-import Upload from './components/Upload/UploadFile';
-import './components/Css/Reset.css';
-
 import './components/Css/Grid.css';
 import './components/Css/Global.css';
-import IntroductoryOffer from './components/IntroductoryOffer/IntroductoryOffer';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
+import Hello from './components/Hello';
 
-import IntroductoryOffer from './components/IntroductoryOffer/IntroductoryOffer';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogged: false,
-      redirectToAdminPage: false,
-    };
-  }
-
-
   
-render() {
-  return (
-    <>
-    <Navbar/>
-      <Switch>
-           <Route exact path="/" component={IntroductoryOffer} /> 
- 
-          {/* <Route exact path="/devis" component={Devis} /> */}
-          <Route exact path="/upload" component={Upload} />
-          <Route exact path="/recrutement" component={Recrutement} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
- 
+  
 
-        <Footer />
+
+
+  render() {
+    return (
+      <>
+        <Hello />
+
+        <Switch>
+        </Switch>
+
+
+
       </>
     );
   }

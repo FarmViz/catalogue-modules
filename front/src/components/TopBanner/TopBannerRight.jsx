c7f289dc7844b53a34480488f3a05880a85b0e5e
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2020-02-25 17:09:20 +0100

    update

--- front/src/components/TopBanner/TopBannerRight.jsx
+++ front/src/components/TopBanner/TopBannerRight.jsx
@@ -1,1 +1,20 @@
-ww
+import React from 'react';
+import '../Css/Global.css';
+import '../Css/Grid.css';
+import './TopBannerRight.css';
+
+class TopBannerRight extends React.Component {
+
+  render() {
+    return (
+     <div className="topbannerleft droite border dsk-6 tab-12 mob-12 " >
+       <div>
+          <a className="center droite couleur" >Service client 6J/7 de 7h à 21h</a>
+          <a className="center texte14b couleur" href="tel:+33223235210"> ☎ 02 24 24 52 10</a>
+       </div>
+    </div>
+    );
+  }
+}
+
+export default TopBannerRight;

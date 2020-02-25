b9fa334ed6a073596ce7a0d694f49116f456f856
Amaury LEBOUTEUX <amaury.lebouteux@gmail.com>
2020-02-25 17:11:46 +0100

    block bannière gauche punch line

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import Hello from './components/Hello';
+import TopBannerLeft from './components/TopBanner/TopBannerLeft';
 
 
 

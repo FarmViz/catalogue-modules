ca774d594bcf6a0b3b32beef264312b71fccb347
rfcniblon <lambinet.nicolas@orange.fr>
2020-02-26 16:39:41 +0100

    correction placement

--- front/src/components/Navbar/NavbarMenu.jsx
+++ front/src/components/Navbar/NavbarMenu.jsx
@@ -12,17 +12,17 @@ class NavbarMenu extends React.Component {
         return (
             // NavbarMenu
          
-                <nav className="dsk-9 tab-8 mob-8 row flex-end">
+                <nav className="dsk-9 tab-8 mob-7 row flex-end ">
                     <input type="checkbox" id="chk" />
                     <label htmlFor="chk" className="show-menu-btn">
                         <i className="fas fa-bars burgercolor"></i>
                     </label>
-                    <ul className="menu dsk-9 NavbarMenuFontSize">
+                    <ul className="menu dsk-8 NavbarMenuFontSize">
                         <li><a href="/">Accueil</a></li>
                         <li><a href="/services">Services</a></li>
                         <li><a href="/societe">Société</a></li>
                         <li><a href="/contact">Contact</a></li>
-                        <label htmlFor="chk" className="hide-menu-btn">
+                        <label htmlFor="chk" className="hide-menu-btn marg">
                             <i className="fas fa-times burgercolor"></i>
                         </label>
                     </ul>

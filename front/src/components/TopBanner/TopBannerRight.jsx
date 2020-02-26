0abcad74cefd0556114a79d054b4d556f7f4b408
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2020-02-26 09:55:19 +0100

    Update TopBannerRight.jsx

--- front/src/components/TopBanner/TopBannerRight.jsx
+++ front/src/components/TopBanner/TopBannerRight.jsx
@@ -9,7 +9,7 @@ class TopBannerRight extends React.Component {
     return (
      <div className="topbannerleft droite border dsk-6 tab-12 mob-12 " >
        <div>
-          <a className="center droite couleur" >Service client 6J/7 de 7h à 21h</a><i class="fas fa-phone"></i> 
+          <a className="center droite couleur" >Service client 6J/7 de 7h à 21h</a><i className="fas fa-phone"></i> 
           <a className="center texte14b couleur" href="tel:+33223235210"> +(33)2 24 24 52 10</a>
        </div>
     </div>

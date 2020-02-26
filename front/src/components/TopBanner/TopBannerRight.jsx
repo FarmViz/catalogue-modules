de56fe8f344cdaf42f945a357f5102658e74f7fc
Amaury LEBOUTEUX <amaury.lebouteux@gmail.com>
2020-02-26 10:52:38 +0100

    bannerTopRight débugg

--- front/src/components/TopBanner/TopBannerRight.jsx
+++ front/src/components/TopBanner/TopBannerRight.jsx
@@ -7,10 +7,10 @@ class TopBannerRight extends React.Component {
 
   render() {
     return (
-     <div className="topbannerleft droite border dsk-6 tab-12 mob-12 " >
+     <div className=" droite border dsk-6 tab-12 mob-12 " >
        <div>
-          <a className="center droite couleur" >Service client 6J/7 de 7h à 21h</a><i class="fas fa-phone"></i> 
-          <a className="center texte14b couleur" href="tel:+33223235210"> +(33)2 24 24 52 10</a>
+          <a className="center texte14Bold paddingRightService couleur" >Service client 6J/7 de 7h à 21h</a><i className="fas fa-phone paddingPhone phoneReverse"></i> 
+          <a className="center texte14Bold couleur paddingNumber" href="tel:+33223235210"> +(33)2 24 24 52 10</a>
        </div>
     </div>
     );

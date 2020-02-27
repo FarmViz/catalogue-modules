6bba7b07dc1316b0205bc9f5898c8c57004f28a2
rfcniblon <lambinet.nicolas@orange.fr>
2020-02-27 16:12:25 +0100

    debut pre commande

---
+++ front/src/components/ProbeOffer/OfferProbePreOrder.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../Css/Global.css';
+import '../Css/Grid.css';
+import './FooterMenu.css';
+
+class OfferProbePreOrder extends Component {
+
+
+    render() {
+        return (
+            <>
+                
+                    <div className="">
+                     
+                    </div>
+                    
+                
+
+            </>
+        )
+    }
+}
+
+
+export default OfferProbePreOrder;
